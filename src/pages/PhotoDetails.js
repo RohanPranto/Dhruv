536e2a2062e72ad665ddebd74ecaf34fe06ce102
Rohan Biswas <72951224+RohanPranto@users.noreply.github.com>
2023-12-02 15:34:00 +0530

    Update PhotoDetails.js
    Img border radius added

--- src/pages/PhotoDetails.js
+++ src/pages/PhotoDetails.js
@@ -34,7 +34,7 @@ function PhotoDetails() {
     <div className="container mt-5">
       <div className="row">
         <div className="col-lg-6">
-          <img src={photo.imageURL} className="img-fluid" alt={photo.caption} />
+          <img src={photo.imageURL} className="img-fluid" style={{borderRadius:"5px"}} alt={photo.caption} />
         </div>
         <div className="col-lg-6">
       <h2 className="mb-4 mt-3">Photo Details</h2>
