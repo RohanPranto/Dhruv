227786b56f7b1ddf1499df96d06af08a2d68753f
Rohan Biswas <rohan.biswas2020@uem.edu.in>
2023-12-26 22:26:02 +0530

    home page image refresh

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../assets/Home.css";
 import Album from "../components/Album";
 import About from "../components/About";
 import Contact from "../components/Contact";
 import home from "../assets/home.jpg";
 import { Link } from "react-router-dom";
+import { collection, getDocs } from "firebase/firestore";
+import { firestore } from "../firebase";
+
 function Home() {
+  const [currentImage, setCurrentImage] = useState(null); // State for image source
+  const [fadeIn, setFadeIn] = useState(true);
+
+  useEffect(() => {
+    const fetchRandomImage = async () => {
+      try {
+        const photoCollection = collection(firestore, "photos");
+        const photosSnapshot = await getDocs(photoCollection);
+        const randomPhoto =
+          photosSnapshot.docs[
+            Math.floor(Math.random() * photosSnapshot.size)
+          ];
+        const photoData = randomPhoto.data();
+
+        // Trigger fade-out effect
+        setFadeIn(false);
+
+        // Wait for the fade-out transition to complete
+        setTimeout(() => {
+          // Set the new image and trigger fade-in effect
+          setCurrentImage(photoData.imageURL);
+          setFadeIn(true);
+        }, 500); // Adjust the delay based on your transition duration
+      } catch (error) {
+        console.error("Error fetching photo:", error);
+      }
+    };
+
+    // Fetch a random image initially
+    fetchRandomImage();
+
+    // Set interval to fetch a new random image every 5 seconds
+    const intervalId = setInterval(() => fetchRandomImage(), 5000);
+
+    // Clear the interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div>
       <div className="container home">
@@ -53,7 +94,9 @@ function Home() {
                   </Link>
                 </button>
                 <a style={{ textDecoration: "none" }} href="#aboout">
-                  <p style={{ marginBottom: 0, paddingTop: 7, fontSize:"1.2em" }}>
+                  <p
+                    style={{ marginBottom: 0, paddingTop: 7, fontSize: "1.2em" }}
+                  >
                     learn more<i className="bx bx-right-arrow-alt"></i>
                   </p>
                 </a>
@@ -61,18 +104,29 @@ function Home() {
             </div>
           </div>
           <div className="col-lg-6">
-            <div style={{padding:14,  border: "2px solid #0157b4" , borderRadius: "10px" }}>
-            <img
-              className="img-fluid"
-              style={{ borderRadius: "8px" }}
-              src={home}
-              alt=""
-            />
+            <div
+              className={`image-container ${fadeIn ? "fade-in" : "fade-out"}`}
+              style={{
+                padding: 14,
+                border: "2px solid #0157b4",
+                borderRadius: "10px",
+              }}
+            >
+              <img
+                className="img-fluid"
+                style={{
+                  borderRadius: "8px",
+                  aspectRatio: "4/3",
+                  objectFit: "cover",
+                }}
+                src={currentImage}
+                alt=""
+              />
             </div>
           </div>
         </div>
       </div>
-      
+
       <Album /> <br />
       <section id="aboout">
         <About />
